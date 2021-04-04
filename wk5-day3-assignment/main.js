e15dbc559de5c3e067dc1a93a2f9427638d4d56c
Yashakk <yashashree.kulkarni05@gmail.com>
2021-04-03 21:58:55 -0400

    first commit

---
+++ wk5-day3-assignment/main.js
@@ -0,0 +1,83 @@
+
+const menu = new Vue({
+    el: "#vue",
+    data: {
+        items: [
+            {
+                id: 1,
+                item: 'Pasta',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Spaghetti_aglio%2C_olio_e_peperoncino_%2816284859030%29.jpg',
+                description: 'Healthy Pasta',
+                
+            },
+            {
+                id: 2,
+                item: 'Red Crab',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Jacques_Lameloise%2C_escab%C3%A8che_d%27%C3%A9crevisses_sur_gaspacho_d%27asperge_et_cresson.jpg',
+                description: 'Fresh from Alaska',
+            },
+            {
+                id: 3,
+                item: 'Breakfast Special',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/0/02/Breakfast_at_Irving_Street_Kitchen.jpg',
+                description: 'Good for the soul',
+                
+            },
+            {
+                id: 4,
+                item: 'Sea Shells Mix',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/4/4b/Seafood_linguine.jpg',
+                description: 'For the shell lovers',
+            },
+            {
+                id: 5,
+                item: 'ultimate kebab',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Various_kebab.jpg',
+                description: 'The best from Middle East',
+            },
+            {
+                id: 6,
+                item: 'chicken steak',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Chicken_steak_with_pepper_sauce.jpg',
+                description: 'Grilled to perfection',
+            },
+            {
+                id: 7,
+                item: 'filet mignon',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/1/1a/.Filet_mignon_et_foie_gras_au_muscat_de_Beaumes-de-Venise%2C_Pastis_Bistro%2C_Palo_Alto.jpg',
+                description: 'Juicy steak served with potatoes and gratin',
+            },
+            {
+                id: 8,
+                item: 'bistro brunch',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Dishes_at_Bistro_C%2C_Hastings_Street%2C_Noosa_Heads%2C_Queensland_02.jpg',
+                description: 'A touch of Australia',
+            },
+            {
+                id: 9,
+                item: 'california crepe',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/3/32/2011-04-09_17.02.43%2C_Whispers_Cafe_and_Creperie%2C_Belmont%2C_CA_%285982715933%29.jpg',
+                description: 'Stuffed with grilled shrimp',
+            },
+            {
+                id: 10,
+                item: 'top burger',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Hamburguesa_de_carne.jpg',
+                description: 'The tallest hamburger from Peru',
+            },
+            {
+                id: 11,
+                item: 'chicken garlic pizza',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Round_Table_chicken_%26_garlic_pizza.JPG',
+                description: 'Our speciality pizza',
+            },
+            {
+                id: 12,
+                item: 'wok away healthy',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/d/d2/Stir_Fry_Wok_-_Free_For_Commercial_Use_-_FFCU_%2827159057131%29.jpg',
+                description: 'Healthy blend of noodles and vegies',
+            },
+
+        ]
+    }
+})
